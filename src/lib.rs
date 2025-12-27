98a210bdcb927839da1cfd0abe57a996fb8f8d03
sinofseven <em.s.00001@gmail.com>
2025-12-27 19:14:25 +0900

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 use worker::{event, Context, Env, Request, Response, Result};
 
 #[event(fetch)]
-async fn fetch(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+async fn fetch(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    let url = req.url()?;
+    let path = url.path();
+
     let payload = serde_json::json!({
-        "message": "Hello World"
+        "message": "Hello World",
+        "path": path.to_string()
     });
     Response::from_json(&payload)
 }
